722923905191973c88775bc9970d0d7463213acf
Alelsito <lbalelxander@gmail.com>
2023-02-21 19:07:43 -0600

    feat: add login form (styles)

---
+++ src/pages/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+// Components
+import LoginForm from '../components/LoginForm'
+
+const Login = () => {
+  return (
+    <>
+      <LoginForm />
+    </>
+  )
+}
+
+export default Login
