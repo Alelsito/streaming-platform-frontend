b1b70877105aaffd87e117ee4dcfe1a586574dff
Alelsito <lbalelxander@gmail.com>
2023-02-22 17:44:24 -0600

    feat: consume movies info from database

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,27 +1,34 @@
 import React from 'react'
 
-const MovieCard = () => {
+const MovieCard = ({ data }) => {
+  const date = new Date(data.dateOfReleased).toLocaleDateString()
+
   return (
     <div className='max-w-sm max-h-10 shadow xs:mt-40 md:mt-40 lg:mt-40 xl:mt-40 2xl:mt-44'>
       <div className='group relative bg-black rounded-md'>
         <img
-          alt=''
-          src='https://media.vogue.mx/photos/63090a31cd86573fdcdd394e/master/w_3000,h_2001,c_limit/the-whale-brendan-fraser.jpg'
+          alt={data.title}
+          src={data.image}
           className='absolute inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-50 rounded-md'
         />
         <div className='relative p-4 2xl:p-6'>
-          <p className='xs:text-sm 2xl:text-sm font-bold uppercase -tracking-normal text-pink-500'>
+          <p className='xs:text-sm 2xl:text-sm font-bold uppercase -tracking-normal text-cyan-600'>
             Movie
           </p>
-          <p className='xs:text-lg 2xl:text-xl font-bold text-white'>The Whale</p>
+          <p className='xs:text-lg 2xl:text-xl font-bold text-white'>
+            {data.title}
+          </p>
           <div className='flex justify-start space-x-4 translate-y-8 transform opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100 mt-2'>
-            <p className='xs:text-sm 2xl:text-base font-medium text-white'>12/02/2023</p>
+            <p className='xs:text-sm 2xl:text-base font-medium text-white'>
+              {date}
+            </p>
             <div className='flex space-x-1'>
-              <p className='xs:text-sm 2xl:text-base font-medium text-white'>6</p>
+              <p className='xs:text-sm 2xl:text-base font-medium text-white'>
+                {data.likes}
+              </p>
               <svg
                 className='text-white pb-1 xs:w-5 xs:h-6 2xl:w-6 2xl:h-7'
                 viewBox='0 0 26 27'
-                stroke-width='2'
                 stroke='currentColor'
                 fill='none'
               >
@@ -33,8 +40,7 @@ const MovieCard = () => {
           <div className='w-80 h-18 pt-2'>
             <div className='translate-y-8 transform opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100'>
               <p className='xs:text-sm text-white'>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis
-                perferendis hic asperiores quibusdam quidem voluptates doloremque
+                {data.description}
               </p>
             </div>
           </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,12 @@ import { useNavigate } from 'react-router-dom'
 // Zustand
 import userContainer from '../config/UserStore'
 
+// Apollo/client
+import { useLazyQuery } from '@apollo/client'
+
+// Queries
+import { GET_MOVIES } from '../graphql/Queries'
+
 // Components
 import MovieCard from '../components/MovieCard'
 
@@ -15,34 +21,22 @@ const Home = () => {
   const getAuthorization = userContainer((state) => state.isAuthorized)
   const isAuthorized = getAuthorization.isAuthorized
 
+  const [getMovies, { data, error }] = useLazyQuery(GET_MOVIES)
+
   useEffect(() => {
     if (isAuthorized !== true) {
       return navigate('/')
-    }
+    } else { getMovies() }
   }, [])
 
   return (
     <div className='flex xs:pt-2 sm:pt-5 md:pt-4 lg:pt-6 xl:pt-5 2xl:pt-2 justify-center flex-wrap gap-x-7 lg:gap-y-3 xl:gap-y-2'>
-      <MovieCard />
-      <MovieCard />
-      <MovieCard />
-      <MovieCard />
-      <MovieCard />
-      <MovieCard />
-      <MovieCard />
-      <MovieCard />
-      <MovieCard />
-      <MovieCard />
-      <MovieCard />
-      <MovieCard />
-      <MovieCard />
-      <MovieCard />
-      <MovieCard />
-      <MovieCard />
-      <MovieCard />
-      <MovieCard />
-      <MovieCard />
-      <MovieCard />
+      {
+        data &&
+          data.getMovies.map((data) => (
+            <MovieCard key={data._id} data={data} />
+          ))
+      }
     </div>
   )
 }
